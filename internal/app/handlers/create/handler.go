9ac0cf14baef70aea724e06afa5948de86d818e1
Semen Viatkin <samvyto@gmail.com>
2024-07-23 00:34:34 +0300

    Add cache to shortener service

--- internal/app/handlers/create/handler.go
+++ internal/app/handlers/create/handler.go
@@ -1,39 +1,58 @@
 package create
 
 import (
+	"io"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
 type Handler struct {
+	cache map[string]string
 }
 
 func New() *Handler {
-	return &Handler{}
+	return &Handler{
+		cache: make(map[string]string),
+	}
 }
 
 func (h *Handler) Handle(res http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case http.MethodGet:
-		getSourceURL(res, req)
+		h.getSourceURL(res, req)
 	case http.MethodPost:
-		createShortURL(res, req)
+		h.createShortURL(res, req)
 	}
 }
 
-func createShortURL(res http.ResponseWriter, req *http.Request) {
+func (h *Handler) createShortURL(res http.ResponseWriter, req *http.Request) {
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	url := string(body)
+	url = strings.TrimSpace(url)
+
+	h.cache[url] = url
+
 	res.Header().Set("Content-Type", "text/plain")
 	res.WriteHeader(http.StatusCreated)
 }
 
-func getSourceURL(res http.ResponseWriter, req *http.Request) {
+func (h *Handler) getSourceURL(res http.ResponseWriter, req *http.Request) {
 	params := strings.Split(req.URL.Path, "/")
 	if ID := findID(params); ID != nil {
-		res.Header().Set("Content-Type", "text/plain")
-		res.Header().Set("Location", "https://google.com")
+		if cached, ok := h.cache[*ID]; ok {
+			res.Header().Set("Content-Type", "text/plain")
+			res.Header().Set("Content-Length", strconv.Itoa(len(cached)))
+			res.Header().Set("Location", cached)
 
-		res.WriteHeader(http.StatusTemporaryRedirect)
-		return
+			res.WriteHeader(http.StatusTemporaryRedirect)
+			return
+		}
 	}
 
 	res.WriteHeader(http.StatusBadRequest)

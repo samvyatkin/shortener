786639e6886ae493f5e09435a9aa6be933c7fbc6
Semen Vyatkin <samvyto@gmail.com>
2024-09-15 14:36:00 +0300

    Refactore file storage

--- internal/app/storage/file_storage.go
+++ internal/app/storage/file_storage.go
@@ -5,12 +5,14 @@ import (
 	"encoding/json"
 	"os"
 	"shortener/internal/app/models"
+	"shortener/internal/app/utils"
+	"strings"
 	"sync"
 )
 
 type FileStorage struct {
-	file  *os.File
-	mutex sync.Mutex
+	path  string
+	mutex *sync.RWMutex
 }
 
 func NewFileStorage(path string) (*FileStorage, error) {
@@ -18,23 +20,22 @@ func NewFileStorage(path string) (*FileStorage, error) {
 		return nil, ErrStorageFilePathEmpty
 	}
 
-	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		return nil, err
-	}
-
 	return &FileStorage{
-		file: file,
+		path:  path,
+		mutex: &sync.RWMutex{},
 	}, nil
-}
-
-func (fs *FileStorage) Close() error {
-	return fs.file.Close()
 }
 
 func (fs *FileStorage) Set(data models.ShortenData) error {
 	fs.mutex.Lock()
 	defer fs.mutex.Unlock()
+
+	file, err := os.OpenFile(fs.path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
 
 	d, err := json.Marshal(data)
 	if err != nil {
@@ -43,7 +44,7 @@ func (fs *FileStorage) Set(data models.ShortenData) error {
 
 	d = append(d, '\n')
 
-	_, err = fs.file.Write(d)
+	_, err = file.Write(d)
 	if err != nil {
 		return err
 	}
@@ -52,35 +53,37 @@ func (fs *FileStorage) Set(data models.ShortenData) error {
 }
 
 func (fs *FileStorage) Get(ID string) (models.ShortenData, error) {
-	if fs.file == nil {
-		return models.ShortenData{}, ErrStorageFileNotExists
+	fs.mutex.RLock()
+	defer fs.mutex.RUnlock()
+
+	file, err := os.OpenFile(fs.path, os.O_RDONLY|os.O_CREATE, 0666)
+	if err != nil {
+		utils.Logger.Errorw("Can't open file %s", fs.path)
+		return models.ShortenData{}, err
 	}
 
-	var d *models.ShortenData
-	scanner := bufio.NewScanner(fs.file)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		line := scanner.Text()
+		line := strings.TrimSuffix(scanner.Text(), "\n")
 		shortenData := models.ShortenData{}
 
 		err := json.Unmarshal([]byte(line), &shortenData)
 		if err != nil {
+			utils.Logger.Errorw("Can't parse object: text - %s err - %w", line, err)
 			return models.ShortenData{}, err
 		}
 
 		if shortenData.ID == ID {
-			d = &shortenData
-			break
+			return shortenData, nil
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
+		utils.Logger.Errorw("Scanner error %w", err)
 		return models.ShortenData{}, err
 	}
 
-	if d == nil {
-		return models.ShortenData{}, ErrStorageValueNotFound
-	}
-
-	return *d, nil
+	return models.ShortenData{}, ErrStorageValueNotFound
 }
